6d3c0d5a2016a2d61dc3a34130a46a473b441db5
Erlend Hall <ehall@equinor.com>
2025-10-13 08:38:14 +0200

    added some exaple records

--- src/ui/NewRecordPanel/NewRecordPanel.tsx
+++ src/ui/NewRecordPanel/NewRecordPanel.tsx
@@ -76,10 +76,7 @@ export function NewRecordPanel() {
                     <li>namespace:master-data--Well:c0ffeeb0-1dea-5e1a-8b0b-deadbeef1234</li>
                     <li>namespace:master-data--BHARun:1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed</li>
                     <li>namespace:master-data--BHARun:6ba7b810-9dad-11d1-80b4-00c04fd430c8</li>
-                    <li>namespace:master-data--BHARun:a828c845-101a-5ca0-a729-84fe19cf8841</li>
                     <li>namespace:master-data--HoleSection:7f24cc1c-5a82-4c7a-98a4-2d1e1c84b3a2</li>
-                    <li>namespace:master-data--HoleSection:9616795e-1fc1-50a2-b40f-e509668b514f</li>
-                    <li>namespace:master-data--TubularAssembly:6494d378-e4df-5ca2-a913-a061a085aada</li>
                     <li>namespace:master-data--TubularAssembly:e91a3b8f-22d5-4e0d-95fc-123456789abc</li>
                     <li>namespace:master-data--Well:4d2e6f12-9c44-41b8-bbff-8a3b14c7d5e1</li>
                     <li>namespace:master-data--Well:6c60ceb0-3521-57b7-9bd8-e1d7c9f66230</li>
