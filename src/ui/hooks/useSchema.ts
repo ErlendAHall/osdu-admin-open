ab4e926994d20a73ee162b7f975084ed723c5d5b
Erlend Hall <ehall@equinor.com>
2026-01-07 07:28:34 +0100

    fixed useSchema

--- src/ui/hooks/useSchema.ts
+++ src/ui/hooks/useSchema.ts
@@ -6,17 +6,13 @@ import { ObjectStores } from "../../types/db.ts";
 
 export function useSchema(kind?: string) {
     const { data, getItem, dbInstance } = useIndexedDb<OSDUSchema>();
-    // kind = '"osdu:wks:master-data--TubularAssembly:2.0.0"';
     // TODO: Use loading state from indexeddb instead.
     const [loading, setIsLoading] = useState(Boolean(data));
 
     useEffectAsync(async () => {
         setIsLoading(true);
         if (!kind) return;
-        await getItem(
-            "osdu:wks:master-data--TubularAssembly:2.0.0",
-            ObjectStores.OSDUSchemaStore
-        );
+        await getItem(kind, ObjectStores.OSDUSchemaStore);
         setIsLoading(false);
     }, [kind, dbInstance]);
 
